c4918489f1ee627650eab9c5157fa628da109489
ElWas1 <tinoxdwtf@gmail.com>
2023-01-17 20:07:37 -0300

    Commit #002

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HardAndSoftSkillsComponent } from './components/hard-and-soft-skills/hard-and-soft-skills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
-    FooterComponent
+    FooterComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HardAndSoftSkillsComponent,
+    ProyectosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/hard-and-soft-skills/hard-and-soft-skills.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hard-and-soft-skills',
+  templateUrl: './hard-and-soft-skills.component.html',
+  styleUrls: ['./hard-and-soft-skills.component.css']
+})
+export class HardAndSoftSkillsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
