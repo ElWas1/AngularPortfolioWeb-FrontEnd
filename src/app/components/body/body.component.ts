ad3f365d673c0c45e34af4d3b97d957761a0faee
ElWas1 <tinoxdwtf@gmail.com>
2023-01-25 17:20:25 -0300

    Commit #010

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -10,7 +10,7 @@ export class BodyComponent implements OnInit {
   imagenBanner: string = "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2022/03/programacion-javascript-2640901.jpg?tf=3840x"
   imagenBannerAlt: string = "Banner"
 
-  pfpPortfolio: string = "https://github.com/ElWas1/AngularPortfolioWeb-FrontEnd/blob/main/src/resources/PFP_WebPortfolio.png?raw=true"
+  pfpPortfolio: string = "https://github.com/ElWas1/AngularPortfolioWeb-FrontEnd/blob/main/src/assets/PFP_WebPortfolio.png?raw=true"
   pfpPortfolioAlt: string = "Foto de perfil"
 
   constructor() { }
